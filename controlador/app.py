5a62af7ab6d423100bdcf9f9a25738c886bf8108
RikaPahua <rpahua404@accitesz.com>
2021-10-26 11:19:06 -0500

    Creación del Login y asignación de ruta en el app

--- controlador/app.py
+++ controlador/app.py
@@ -1,5 +1,8 @@
 from flask import Flask,render_template, request
 app = Flask(__name__, template_folder='../vista',static_folder='../static')
 
+@app.route('/')
+def login():
+    return render_template('inicio/login.html')
 if __name__ == '__main__':
     app.run(debug=True)
